32ad0773e1511105e7fc99e7e9396aa6c50f27a0
Andrew Matthews <andrew.matthews@team.telstra.com>
2020-07-22 21:11:51 +1000

    added ext to definition

--- functions/__fd2_chkl_define.fish
+++ functions/__fd2_chkl_define.fish
@@ -1,7 +1,8 @@
-# Defined in /home/d235108/.config/fishdots/plugins/fishdots_checklist/init/rc1.d/01-functions.fish @ line 105
-function __fd2_chkl_define --description 'creates a new definition of a checklist' --argument name
-  set -l slug (__fd2_to_slug $name)
-    set -l target_path "$fd2_checklist_definitions_home/$slug"
+function __fd2_chkl_define --description 'creates a new definition of a checklist' 
+  set -l slug (__fd2_to_slug "$argv")
+  echo slug is $slug
+  set -l target_path "$fd2_checklist_definitions_home/$slug.md"
+  echo path is $target_path
     # if the definition already exists, edit that instead
     if test -e $target_path
         set -U fd2_checklist_current_definition "$target_path"
