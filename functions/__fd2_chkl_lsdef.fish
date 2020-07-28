fe48e3ab76bf209520f69309a4cfe0528bcee6ce
Andrew Matthews <andrew.matthews@team.telstra.com>
2020-07-29 09:56:39 +1000

    cleaner lsdef

--- functions/__fd2_chkl_lsdef.fish
+++ functions/__fd2_chkl_lsdef.fish
@@ -1,7 +1,7 @@
 function __fd2_chkl_lsdef
         set results (__fd2_find $fd2_checklist_definitions_home "*.md")
         set ctr 1
-        for r in results
+        for r in $results
                 echo $ctr (basename $r)
                 set ctr (math $ctr + 1)
         end
