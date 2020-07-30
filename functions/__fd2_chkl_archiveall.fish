1e215a1ffbdca4fb4cb798626f9e1d6c72b83a9a
Andrew Matthews <andrew.matthews@team.telstra.com>
2020-07-31 09:22:44 +1000

    updated to new subcmd format

--- functions/__fd2_chkl_archiveall.fish
+++ functions/__fd2_chkl_archiveall.fish
@@ -1,5 +1,5 @@
 function __fd2_chkl_archiveall --description 'archive all active checklists'
-  for inst in (__fd2_find $fd2_checklist_instances_home "*.md")
+  for inst in (fd2_find $fd2_checklist_instances_home "*.md")
     __fd2_chkl_archive_inst $inst
   end
 end
--- functions/__fd2_chkl_lsdef.fish
+++ functions/__fd2_chkl_lsdef.fish
@@ -1,5 +1,5 @@
 function __fd2_chkl_lsdef
-        set results (__fd2_find $fd2_checklist_definitions_home "*.md")
+        set results (fd2_find $fd2_checklist_definitions_home "*.md")
         set ctr 1
         for r in $results
                 echo -e "$ctr\t"(basename $r)
